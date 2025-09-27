5c1614c421c2d8f56088d0e0ca22b6db91d64ac9
HUSTLER <averin.ilya@inbox.ru>
2025-09-27 21:24:46 +0500

    fix

--- src/world/UnitBehavior.js
+++ src/world/UnitBehavior.js
@@ -15,15 +15,50 @@ class UnitBehavior extends Behavior
     update() {
         super.update ();
 
+        let target = {x:0,y:0}
 
-        if(this.unit.position.x)
+        if(this.unit.position.x < 3 && this.unit.position.y < 3)
+        {
+            target = {x:49,y:1}
+        }
 
+        if(this.unit.position.x > 48 && this.unit.position.y < 2)
+        {
+            target = {x:49,y:49}
+        }
 
+        if(this.unit.position.x > 48 && this.unit.position.y > 48)
+        {
+            target = {x:0,y:0}
+        }
 
-
-
-
+        this.moveTo(target.x, target.y)
     }
+
+    moveTo(x, y) {
+        let pos_x = this.unit.position.x;
+        let pos_y = this.unit.position.y;
+
+        // Вектор до цели
+        let dx = x - pos_x;
+        let dy = y - pos_y;
+
+        // Длина (расстояние до цели)
+        let dist = Math.sqrt(dx*dx + dy*dy);
+
+        if (dist > 0.01) { // чтобы не "дрожал" около цели
+            let speed = 0.1; // скорость передвижения
+
+            // Нормализованный вектор движения
+            let vx = dx / dist;
+            let vy = dy / dist;
+
+            // Двигаем юнит
+            this.unit.position.x += vx * speed;
+            this.unit.position.y += vy * speed;
+        }
+    }
+
 
 
 }
